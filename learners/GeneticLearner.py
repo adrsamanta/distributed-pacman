e556f65e4dbf8b504cdd201a015b54607f5fa575
adrsamanta <alan.samanta@gmail.com>
2016-09-27 17:48:01 -0500

    FINALLY have basic pop stuff set up

--- learners/GeneticLearner.py
+++ learners/GeneticLearner.py
@@ -5,8 +5,25 @@ import LearnBase
 
 NUM_FEAT = len(LearnBase.LearnerBase.Features._fields)
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("WeightVec", list, fitness=creator.FitnessMax)
+
+class Team:
+    def __init__(self, off, defe):
+        self.offense = off
+        self.defense = defe
+
+
+# weight structure:
+# (score_weight, Amt of food eaten by offensive agent, amt of food eaten by enemy)
+
+creator.create("ScoreMax", base.Fitness, weights=(1.0, 0.0, 0.0))
+# teams optimized with below fitnesses will only care about how well the offensive or defensive agent does
+# so when picking best offensive/defensive agents, just pick best teams with these fitnesses
+creator.create("AteFoodMax", base.Fitness, weights=(0., 1., 0.))
+creator.create("EFoodMin", base.Fitness, weights=(0., 0., -1.))
+
+creator.create("ScoreTeam", Team, fitness=creator.ScoreMax)
+creator.create("OffenseTeam", Team, fitness=creator.AteFoodMax)
+creator.create("DefenseTeam", Team, fitness=creator.EFoodMin)
 
 toolbox = base.Toolbox()
 
@@ -14,9 +31,8 @@ toolbox.register("attr_float", random.uniform, -1, 1)
 toolbox.register("attr_pos_float", random.random)
 toolbox.register("attr_neg_float", random.uniform, -1, 0)
 
-toolbox.register("init_wv", tools.initRepeat, creator.WeightVec,
+toolbox.register("init_wv", tools.initRepeat, list,
                  toolbox.attr_float, n=NUM_FEAT)
-
 
 def create_seeded_ind(ind_init):
     ind = tools.initRepeat(list, toolbox.attr_pos_float, NUM_FEAT)
@@ -27,18 +43,42 @@ def create_seeded_ind(ind_init):
     return ind_init(ind)
 
 
+# should agent 1 be seeded, should agent 2 be seeded, what team are we using
+
+def create_same_team(seed, team):
+    return create_team(seed, seed, team)
+
+
+def create_team(seeded1, seeded2, team):
+    if seeded1:
+        off = create_seeded_ind(list)
+    else:
+        off = toolbox.init_wv()
+
+    if seeded2:
+        defe = create_seeded_ind(list)
+    else:
+        defe = toolbox.init_wv()
+
+    return team(off, defe)
+
+
+toolbox.register("create_score_team", create_same_team, team=creator.ScoreTeam)
+toolbox.register("create_offense_team", create_same_team, team=creator.OffenseTeam)
+toolbox.register("create_defense_team", create_same_team, team=creator.DefenseTeam)
+
 # create a population with some "seeded" individuals (pre-determined signs)
-def initPopulation(pcls, ind_init, n_seed, n_rand):
-    pop = [create_seeded_ind(ind_init) for i in range(n_seed)]
-    rand_pop = [toolbox.init_wv() for i in range(n_rand)]
+def initPopulation(pcls, team, n_seed, n_rand):
+    pop = [team(True) for i in range(n_seed)]
+    rand_pop = [team(False) for i in range(n_rand)]
     pcls(pop + rand_pop)
+
 
 
 N_SEEDED = 10
 N_RAND = 10
 POP = 20
 
-# to train together, pass in just 1 weight vector, to train apart, pass in 2
-
-toolbox.register("seeded_pop", initPopulation, list, creator.WeightVec, N_SEEDED, N_RAND)
-toolbox.register("pop", tools.initRepeat, list, creator.WeightVec, n=POP)
+# pops for score team
+toolbox.register("seeded_pop", initPopulation, list, toolbox.create_score_team, N_SEEDED, N_RAND)
+toolbox.register("pop", tools.initRepeat, list, creator.create_score_team, n=POP)
