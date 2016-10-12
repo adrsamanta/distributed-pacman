9432d7e901f27d2c4999618a01fca14c2b83c3ca
adrsamanta <alan.samanta@gmail.com>
2016-10-12 14:09:41 -0500

    lol added the write part

--- run_several.py
+++ run_several.py
@@ -4,8 +4,8 @@ program = "python -m scoop GeneticLearner.py -ngen 16 -s 10 -r 5"
 prefix = "OEL/10.12/"
 
 for i in range(5):
-    outfile = open(prefix + str(i) + "out.log")
-    errfile = open(prefix + str(i) + "err.log")
+    outfile = open(prefix + str(i) + "out.log", "w")
+    errfile = open(prefix + str(i) + "err.log", "w")
     subprocess.call(program, stdout=outfile, stderr=errfile)
     print "finished call", i
 
