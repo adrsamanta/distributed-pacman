0881620c0fbb600f5ead360eb248942193b31630
adrsamanta <alan.samanta@gmail.com>
2016-10-12 14:15:01 -0500

    added shell=True

--- run_several.py
+++ run_several.py
@@ -6,7 +6,7 @@ prefix = "OEL/10.12/"
 for i in range(5):
     outfile = open(prefix + str(i) + "out.log", "w")
     errfile = open(prefix + str(i) + "err.log", "w")
-    subprocess.call(program, stdout=outfile, stderr=errfile)
+    subprocess.call(program, stdout=outfile, stderr=errfile, shell=True)
     print "finished call", i
 
 print "done"
