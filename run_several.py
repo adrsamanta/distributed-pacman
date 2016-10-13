24a2720559a4122d57dd835a75262398acb2243e
adrsamanta <alan.samanta@gmail.com>
2016-10-13 00:39:41 -0400

    updated run several

--- run_several.py
+++ run_several.py
@@ -1,9 +1,20 @@
 import subprocess
+from datetime import datetime
+
+import argparse
+
+today_folder = '{:%m.%d}'.format(datetime.now())
 
 program = "python -m scoop GeneticLearner.py -ngen 16 -s 10 -r 5"
-prefix = "OEL/10.12/"
+prefix = "OEL/" + today_folder + "/"
 
-for i in range(5):
+parser = argparse.ArgumentParser()
+
+parser.add_argument("num_rounds", type=int)
+
+args = parser.parse_args()
+rounds = args.num_rounds
+for i in range(rounds):
     outfile = open(prefix + str(i) + "out.log", "w")
     errfile = open(prefix + str(i) + "err.log", "w")
     subprocess.call(program, stdout=outfile, stderr=errfile, shell=True)
