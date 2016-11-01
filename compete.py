51632938640caab7505e3d1708274693acbd7dc4
adrsamanta <alan.samanta@gmail.com>
2016-11-01 15:40:12 -0500

    changed compete around sum

--- compete.py
+++ compete.py
@@ -60,20 +60,32 @@ def run_game(score, offe, defe, red_gen=True, blue_gen=True):
 
     score_food_list = capture.main_run(red_team + blue_team + red_opts + blue_opts + game_opts)
     labels = ["score", "food eaten", "enemy food eaten"]
+
     for sfl in score_food_list:
         print("Score: ", sfl[0])
-        for l, v in zip(labels, sfl[1]):
-            print(l, v, end="; ")
+        print("Red Food: ", sfl[1][0] + sfl[1][2])
+        print("Blue Food: ", sfl[1][1] + sfl[1][3])
 
-        print()
+    # return the average score over all games
+    return sum(sfl[0] for sfl in score_food_list) / len(score_food_list)
 
 
-for i in range(10):
+def make_team(offe, defe):
+    return creator.ScoreTeam(offe.offense, defe.defense)
+
+
+rounds = 10
+score_scores = 0
+composed_scores = 0
+for i in range(rounds):
     score = random.choice(scoreAgents)
+    score_scores += run_game(score, None, None, blue_gen=False)
     offe = random.choice(offenseAgents)
     defe = random.choice(defenseAgents)
-    run_game(score, offe, defe)
+    composed_scores += run_game(make_team(offe, defe), None, None, blue_gen=False)
     print('\n\n')
 
+print("Average score team score: ", score_scores / rounds)
+print("Average composed score: ", composed_scores / rounds)
 
     # run_game(bestScore, bestOffense, bestDefense, True, True)
